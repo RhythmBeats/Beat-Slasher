9f6a7196af3acd8678cb5dfad12fd7b7effd343e
Jaeyoung Kang <74623611+j97kang@users.noreply.github.com>
2021-01-04 15:13:08 -0500

    spawn scripts

---
+++ light Work/Assets/scripts/Spawn_6.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn_6 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject myPrefab;
+
+    // This script will simply instantiate the Prefab when the game starts.
+    void Start()
+    {
+        // Instantiate at position (0, 0, 0) and zero rotation.
+        Instantiate(myPrefab, new Vector3((float)-1.18, (float)-2.06, 0), Quaternion.identity);
+    }
+}
