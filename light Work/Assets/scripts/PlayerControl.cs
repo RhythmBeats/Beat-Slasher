4316b0cc2973c0d677f81a646f6368976083a5c5
Joseph <joemitch4848@gmail.com>
2021-01-05 00:01:19 -0330

    Player Control stuff for Yazan

--- light Work/Assets/scripts/PlayerControl.cs
+++ light Work/Assets/scripts/PlayerControl.cs
@@ -6,7 +6,10 @@ public class PlayerControl : MonoBehaviour
 {
     public Animator animator;
 
-    public KeyCode key;
+    public KeyCode key1;
+    public KeyCode key2;
+
+    public bool sDL = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,13 +18,22 @@ public class PlayerControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(key))
+        if (Input.GetKeyDown(key1) && animator.GetBool("SlashDownLeft") == false)
         {
             animator.SetBool("SlashDownLeft", true);
         }
-        if (Input.GetKeyUp(key))
+        else
         {
-            animator.SetBool("SlashDownLeft", true);
+            animator.SetBool("SlashDownLeft", false);
         }
+        if (Input.GetKeyDown(key2) && animator.GetBool("SlashDownRight") == false)
+        {
+            animator.SetBool("SlashDownRight", true);
+        }
+        else
+        {
+            animator.SetBool("SlashDownRight", false);
+        }
+           
     }
 }
