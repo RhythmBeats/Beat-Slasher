7e0e436599a91a8cba710f47623edb0923f1dea4
Yazan Masoud <41130598+yaxan@users.noreply.github.com>
2021-01-07 01:53:34 -0500

    Beats and fixed shuriken deletion
    first beatmap set in place, shurikens don't rubber band

--- light Work/Assets/scripts/ShurikenFly.cs
+++ light Work/Assets/scripts/ShurikenFly.cs
@@ -6,28 +6,34 @@ public class ShurikenFly : MonoBehaviour
 {
     public float speed;
 
+    public float speedRotate;
+
     public GameObject spawnPoint;
 
     Vector2 targetPosition = new Vector2(0, 0);
 
     public Vector2 originalPosition;
 
+    private float startTime;
+
     // Start is called before the first frame update
     void Start()
     {
         originalPosition = transform.position;
+        
+        startTime = Time.time;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if ((Vector2)transform.position != targetPosition)
+        if (Time.time - startTime < 4f/3f)
         {
             transform.position = Vector2.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+             if(this.gameObject.transform.childCount > 0) transform.GetChild(0).Rotate(-Vector3.forward*2);
         }
-        else
-        {
-            //animator.Play("Skeleton Dead");
+        else{
             Destroy(gameObject);
         }
     }
